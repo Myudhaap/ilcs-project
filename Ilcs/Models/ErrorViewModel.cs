4f32879de4a07aecebb34816427d01f7d3181fed
yudhaPratama <muhyudhapratama55@gmail.com>
2024-04-20 09:57:36 +0700

    ILCS PROJECT

---
+++ Ilcs/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Ilcs.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ Ilcs/Models/IlcsViewModel.cs
@@ -0,0 +1,10 @@
+using System.Numerics;
+
+class IlcsViewModel{
+    public string Negara {get;set;}
+    public string Pelabuhan {get;set;}
+    public string Barang {get;set;}
+    public string BarangDescription {get;set;}
+    public BigInteger Harga {get;set;}
+    public int TarifBeaCukai {get;set;}
+}
